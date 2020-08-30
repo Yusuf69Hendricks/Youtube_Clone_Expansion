57ed337b5cbe3deba4551ae294ac2646114843ae
Yusuf69Hendricks <yusuf69hendricks@gmail.com>
2020-08-30 17:49:16 +0200

    added sttyling for mobile responsiveness

---
+++ main.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function dropFunction(x) {
+  x.classList.toggle("fa-angle-down");
+}
